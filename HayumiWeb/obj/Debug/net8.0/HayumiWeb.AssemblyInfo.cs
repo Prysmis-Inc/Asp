f12e7455ca7a65a2501e47b5d57811214f145efa
Renan-Nishitani <renanmitsuru@gmail.com>
2024-10-22 00:16:02 -0300

    alterações

--- HayumiWeb/obj/Debug/net8.0/HayumiWeb.AssemblyInfo.cs
+++ HayumiWeb/obj/Debug/net8.0/HayumiWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HayumiWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a854247e833317be84c127316a0da2495d9385f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e37db742094b14650142315384e64a957e474a1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HayumiWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HayumiWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
