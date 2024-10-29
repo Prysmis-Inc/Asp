e8c87bc0cd46d30192f0dafa0a2eb4c85837b0d3
Sky Cloud <cloud.devs2024@gmail.com>
2024-10-29 14:54:04 -0300

    Alterações

--- HayumiWeb/obj/Debug/net8.0/HayumiWeb.AssemblyInfo.cs
+++ HayumiWeb/obj/Debug/net8.0/HayumiWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HayumiWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f12e7455ca7a65a2501e47b5d57811214f145efa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dfafaf30fb8dc2e66ed6fee9954a169093e1bb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HayumiWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HayumiWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
